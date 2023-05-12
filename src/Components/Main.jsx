5c1dc0440df81f252be8825ba2db50545c32d459
Satch5918 <satch_elfante@hotmail.com>
2023-05-12 18:41:26 -0300

    main and foot

---
+++ src/Components/Main.jsx
@@ -0,0 +1,11 @@
+
+
+function Main() {
+  return (
+    <main className="bg-white p-4 my-10">
+      {/* Contenido en proceso */}
+    </main>
+  );
+}
+
+export default Main;
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import Main from "../Components/main";
 
 const Layout = () => {
   return(
     <>
       <Navbar/>
       <Outlet/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
